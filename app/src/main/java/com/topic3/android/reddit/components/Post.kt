f4ccaa6bd65ca8a5130eaa5802ac7c09da5e9ba2
krosbite <killer1233215@mail.ru>
2023-05-10 01:50:56 +0300

    toast

--- app/src/main/java/com/topic3/android/reddit/components/Post.kt
+++ app/src/main/java/com/topic3/android/reddit/components/Post.kt
@@ -28,28 +28,34 @@ import com.topic3.android.reddit.domain.model.PostModel
 import com.topic3.android.reddit.domain.model.PostModel.Companion.DEFAULT_POST
 
 @Composable
-fun TextPost(post: PostModel) {
-    Post(post) {
-        TextContent(post.text)
-    }
+fun TextPost(
+    post: PostModel,
+    onJoinButtonClick: (Boolean) -> Unit = {}
+) {
+    Post(post, onJoinButtonClick){ TextContent(post.text) }
 }
 
 @Composable
-fun ImagePost(post: PostModel) {
-    Post(post) {
-        ImageContent(post.image ?: R.drawable.compose_course)
-    }
+fun ImagePost(
+    post: PostModel,
+    onJoinButtonClick: (Boolean) -> Unit={}
+) {
+    //Post(post, onJoinButtonClick){ ImageContent(post.image!!)}
 }
 
 @Composable
-fun Post(post: PostModel, content: @Composable () -> Unit = {}) {
+fun Post(
+    post: PostModel,
+    onJoinButtonClick: (Boolean) -> Unit = {},
+    content: @Composable () -> Unit = {}
+) {
     Card(shape = MaterialTheme.shapes.large) {
         Column(
             modifier = Modifier.padding(
                 top = 8.dp, bottom = 8.dp
             )
         ) {
-            Header(post)
+            Header(post, onJoinButtonClick)
             Spacer(modifier = Modifier.height(4.dp))
             content.invoke()
             Spacer(modifier = Modifier.height(8.dp))
@@ -58,6 +64,7 @@ fun Post(post: PostModel, content: @Composable () -> Unit = {}) {
     }
 
 }
+
 
 @Composable
 fun Header(
