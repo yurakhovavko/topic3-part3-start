f13a137dc3507279c4a980d09c27d1943e3e05c2
krosbite <killer1233215@mail.ru>
2023-05-10 01:02:26 +0300

    backhandler

--- app/src/main/java/com/topic3/android/reddit/screens/ChooseCommunityScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/ChooseCommunityScreen.kt
@@ -1,19 +1,30 @@
 package com.topic3.android.reddit.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.topic3.android.reddit.R
+import com.topic3.android.reddit.routing.BackButtonAction
+
 import com.topic3.android.reddit.routing.RedditRouter
 import com.topic3.android.reddit.viewmodel.MainViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 
 private const val SEARCH_DELAY_MILLIS = 300L
 
@@ -21,7 +32,50 @@ private val defaultCommunities = listOf("raywenderlich", "androiddev", "puppies"
 
 @Composable
 fun ChooseCommunityScreen(viewModel: MainViewModel, modifier: Modifier = Modifier) {
-    //TODO Add your code here
+    val scope = rememberCoroutineScope()
+    val communities: List<String> by viewModel.subreddits.observeAsState(emptyList())
+    var searchedText by remember { mutableStateOf("") }
+    var currentJob by remember { mutableStateOf<Job?>(null) }
+    val activeColor = MaterialTheme.colors.onSurface
+
+    LaunchedEffect(Unit) {
+        viewModel.searchCommunities(searchedText)
+    }
+
+    Column {
+        ChooseCommunityTopBar()
+        TextField(
+            value = searchedText,
+            onValueChange = {
+                searchedText = it
+                currentJob?.cancel()
+                currentJob = scope.async {
+                    delay(SEARCH_DELAY_MILLIS)
+                    viewModel.searchCommunities(searchedText)
+                }
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Default.Search,
+                    contentDescription = stringResource(id = R.string.search)
+                )
+            },
+            label = { Text(stringResource(R.string.search)) },
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = activeColor,
+                focusedLabelColor = activeColor,
+                cursorColor = activeColor,
+                backgroundColor = MaterialTheme.colors.surface
+            )
+        )
+        SearchedCommunities(communities, viewModel, modifier)
+    }
+    BackButtonAction {
+        RedditRouter.goBack()
+    }
 }
 
 @Composable
@@ -30,14 +84,15 @@ fun SearchedCommunities(
     viewModel: MainViewModel?,
     modifier: Modifier = Modifier
 ) {
-    communities.forEach {Community(
-        text = it,
-        modifier = modifier,
-        onCommunityClicked = {
-            viewModel?.selectedCommunity?.postValue(it)
-            RedditRouter.goBack()
-        }
-    )
+    communities.forEach {
+        Community(
+            text = it,
+            modifier = modifier,
+            onCommunityClicked = {
+                viewModel?.selectedCommunity?.postValue(it)
+                RedditRouter.goBack()
+            }
+        )
     }
 }
 
@@ -71,4 +126,16 @@ fun ChooseCommunityTopBar(modifier: Modifier = Modifier) {
             .height(48.dp)
             .background(Color.Blue)
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchedCommunitiesPreview(){
+    Column {
+        SearchedCommunities(
+            defaultCommunities,
+            null,
+            Modifier
+        )
+    }
 }
